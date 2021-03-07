bade8ef162694154405f22b2aab1551f77281a24
Joao Vicente <joao@upsites.digital>
2021-03-07 12:10:14 -0300

    Aprendendo sobre metodo construtor. Criamos os atributos de instância, nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('João')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Luciano'
+    print(p.nome)
