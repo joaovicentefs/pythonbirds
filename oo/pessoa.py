41207a96116a9850b3146cf4fb58f7ce36d10a66
Joao Vicente <joao@upsites.digital>
2021-03-09 23:00:55 -0300

    Criação de atributos de Classe. Devemos sempre nos perguntar: O atributo será uado em todos os objetos? Se sim devfemos usar o Atrinbuto de Classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
@@ -20,5 +21,10 @@ if __name__ == '__main__':
         print(filho.nome)
     joao.sobrenome = 'Farias'
     del joao.filhos
+    joao.olhos = 1
     print(joao.__dict__)
     print(gregorio.__dict__)
+    print(Pessoa.olhos)
+    print(joao.olhos)
+    print(gregorio.olhos)
+    print(id(Pessoa.olhos), id(joao.olhos), id(gregorio.olhos))
